2c6486fd6dcb5c3a05d1fde99630e4c6f2bf8374
Yoshinari Fujinuma <fujinumay@gmail.com>
2014-12-24 23:37:57 +0900

    bug fixed, stopping criteria included

--- logisticRegression.py
+++ logisticRegression.py
@@ -5,44 +5,63 @@ from math import log
 def sigmoid(x, theta):
     return 1.0 / (1.0 + np.exp(-np.dot(theta.transpose(), x)))
 
-def cost(x, y):
-    return -y * log(sigmoid(x)) - (1 - y) * log(1 - sigmoid(x))
-
 class LogisticRegression():
     """ Training is done using gradient descent
         See http://cs229.stanford.edu/notes/cs229-notes1.pdf for how the gradient is computed.
     """
     
     def __init__(self, X, Y):
+        """ N: num. of training data
+            d: dimention of features
+        """
         self.d = len(X[1,:])
         self.N = len(X[:,1])
         self.X_train = X
         self.Y_train = Y
 
-    def update_matrix(self, thetas, alpha=0.1):
+    def update_matrix(self, thetas, alpha=0.1, C=1):
+        """ C: regularization parameter
+        """
         new_thetas = np.zeros((self.d))
         for i in range(self.N):
-            new_thetas = thetas + alpha * (self.Y_train[i] - sigmoid(self.X_train[i,:], thetas)) * self.X_train[i,:]
+            new_thetas = thetas + alpha * (self.Y_train[i] - sigmoid(self.X_train[i,:], thetas)) * self.X_train[i,:] - C * thetas
 
         return new_thetas
 
 
-    def update(self, thetas, alpha=0.1):
+    def update(self, thetas, alpha=0.01, C=1):
+        """ C: regularization parameter
+        """
         new_thetas = np.zeros((self.d))
         for i in range(self.N):
             for j in range(thetas.size):
-                new_thetas[j] = thetas[j] - alpha * (sigmoid(self.X_train[i, :], thetas) - self.Y_train[i]) * self.X_train[i, j]
+                new_thetas[j] = thetas[j] - alpha * (sigmoid(self.X_train[i, :], thetas) - self.Y_train[i]) * self.X_train[i, j] - C * thetas[j]
 
         return new_thetas
 
+
+    def converged(self, thetas, new_thetas):
+        s = 0
+        for i in range(len(thetas)):
+            s += abs(thetas[i] - new_thetas[i])
+        
+        if (1.0 * s) / len(thetas) < 0.000001:
+            return True
+        return False
+
+
     def train(self, alpha=0.1):
-        thetas = np.zeros(self.d)
-        new_thetas = thetas + 1
-        while thetas.all() != new_thetas.all():
-            #tmp_new_thetas = self.update(old_thetas)
-            tmp_new_thetas = self.update_matrix(thetas)
+        thetas = np.zeros(self.d) # initialization
+        new_thetas = np.zeros(self.d) + 0.01 # initialization
+        # if you initialize with zeroes, then the regularization parameter will be ignored
+        i = 0
+        while i < 100 and not self.converged(thetas, new_thetas):
+            print "thetas before update: " + str(thetas)
             thetas = new_thetas
-            new_thetas = tmp_new_thetas
+            new_thetas = self.update(thetas)
+            #new_thetas = self.update_matrix(thetas)
+            print "thetas after update: " + str(new_thetas)
+            i += 1
         return new_thetas
 
 
@@ -51,14 +70,14 @@ if __name__ == '__main__':
                         [2, 4, 8], 
                         [2, 4, 8], 
                         [2, 2, 2], 
+                        [0, 4, 9], 
                         [2, 5, 9]])
     Y_train = np.array([[1],
                         [1], 
                         [1], 
                         [0], 
+                        [1], 
                         [1]])
 
     LR = LogisticRegression(X_train, Y_train)
-    #d = 3 dimension of each data
-    #N = 5 num. of training data
-    print LR.train()
+    LR.train()
